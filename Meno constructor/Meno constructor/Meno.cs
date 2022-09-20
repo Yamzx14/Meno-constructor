465928b7c4a691d719bcf3cd969853ec9fa32700
Yamzx14 <99759853+Yamzx14@users.noreply.github.com>
2022-09-19 22:35:43 -0500

    Add files via upload

---
+++ Meno constructor/Meno constructor/Meno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meno_constructor
+{
+    
+    internal class Menoclass
+    {
+        //campo propiedades, como es
+        private int n1;
+        private int n2;
+
+       
+        public Menoclass (int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+
+       
+        public int menos()
+        {
+            return (n1 - n2);
+
+        }
+    }
+}
